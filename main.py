aabde74d5beb1d06ab74fb93b892a536a960febf
yossefGaravani <118227495+yossefGaravani@users.noreply.github.com>
2023-08-22 16:54:08 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+print(pow(10, 4))
+print(abs(-90))
+
+import math
+
+hi = math.sqrt(16)
+print(hi)
+print(round(hi))
+x = 12 * 12122.099999
+print(round(x, 2))
+
+import random
+
+hi = random.random()
+bi = random.randint(1, 8)
+di = random.uniform(1, 19)
+print(hi)
+print(bi)
+print(di)
+
+numbers = [1, 2, 3, 4, 5]
+si = random.choice(numbers)
+print(si)
+
+gi = random.sample(numbers, 3)
+print(gi)
+
+num = int(input("enter your num"))
+wage = 30
+print(wage + num)
+
